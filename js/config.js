7ff8fd2fa7ccb6e9a443df6b1da3231330920b28
Your Name <ashiksino84@gmail.com>
2026-04-21 08:25:34 +0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const telegramBotToken1 = '7806063795:AAHVrxzMdB9Vn-yBHvcTO0Kk1JB9itad-7M ';
+const bot1ChatId = '6657550259';
+
+const telegramBotToken2 = '7806063795:AAHVrxzMdB9Vn-yBHvcTO0Kk1JB9itad-7M ';
+const bot2ChatId = '6657550259';
